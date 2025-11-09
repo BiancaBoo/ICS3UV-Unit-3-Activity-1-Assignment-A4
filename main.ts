3260aa4aacb74a799cc73f89a13af0883153345f
default <bianca.bertinato@stu.ocsb.ca>
2025-11-09 15:52:57 +0000

    added average of three numbers

---
+++ main.ts
@@ -0,0 +1,21 @@
+/*
+* Author: Bianca Bertinato
+* Version: 1.0.0
+* Date: 2025-11-09
+* This program displays the average of three numbers .
+*/
+
+// INITIALIZE
+const NUMBER1: number = 56.9;
+const NUMBER2: number = 89.7;
+const NUMBER3: number = 90.2;
+
+// PROCESS
+// calculate the average
+let answer: number = ((NUMBER1 + NUMBER2 + NUMBER3)/3);
+
+// OUTPUT
+// display the result
+console.log("The average of " + NUMBER1 + ", " + NUMBER2 + ", and " + NUMBER3 + " is " + answer);
+
+console.log ("\nDone.");
